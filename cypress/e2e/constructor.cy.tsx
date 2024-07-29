fe93293021c4302df8f0448464c4b74f046d060e
DavidKhlutkov <davidhlutkov2k18@gmail.com>
2024-07-29 19:46:31 +0300

    fix for review save

--- cypress/e2e/constructor.cy.tsx
+++ cypress/e2e/constructor.cy.tsx
@@ -1,44 +1,47 @@
-const ingredients = '[data-cy=burger-ingredients]';
-const bun = '[data-cy=ingredient-bun]';
-const sauce = '[data-cy=ingredient-sauce]';
-const main = '[data-cy=ingredient-main]';
-const testUrl = 'http://localhost:4000';
+const ingredientsBurger = '[data-cy=burger-ingredients]';
+const bun = 'Краторная булка N-200i';
+const sauce = 'Соус Spicy-X';
+const main = 'Мясо бессмертных моллюсков Protostomia';
 const closeModal = '[data-cy=modal-close-button]';
 const submit = '[data-cy=order-submit]';
+const add = 'Добавить';
+const ditals = 'Детали ингредиента';
 
 describe('Constructor', () => {
   beforeEach(() => {
     cy.intercept('GET', 'api/ingredients', { fixture: 'ingredients.json' }).as(
       'ingredients'
     );
-    cy.visit(testUrl);
+    cy.visit('/');
     cy.wait('@ingredients');
   });
   it('should add ingredient to constructor', () => {
-    cy.get(bun).contains('Добавить').click();
-    cy.get(sauce).contains('Добавить').click();
-    cy.get(main).contains('Добавить').click();
-    cy.get(ingredients).contains('Краторная булка N-200i').should('exist');
-    cy.get(ingredients)
-      .contains('Биокотлета из марсианской Магнолии')
-      .should('exist');
+    cy.get(bun).contains(add).click();
+    cy.get(sauce).contains(add).click();
+    cy.get(main).contains(add).click();
+    cy.get(ingredientsBurger).contains(bun).should('exist');
+    cy.get(ingredientsBurger).contains(sauce).should('exist');
+    cy.get(ingredientsBurger).contains(main).should('exist');
   });
 });
 
 describe('Test modal', () => {
   beforeEach(() => {
-    cy.intercept('GET', 'api/ingredients', { fixture: 'ingredients.json' });
-    cy.visit(testUrl);
+    cy.intercept('GET', 'api/ingredients', { fixture: 'ingredients.json' }).as(
+      'ingredients'
+    );
+    cy.visit('/');
+    cy.wait('@ingredients');
   });
   it('should open modal', () => {
-    cy.contains('Детали ингредиента').should('not.exist');
+    cy.contains(ditals).should('not.exist');
     cy.contains('Биокотлета из марсианской Магнолии').click();
   });
   it('should close modal', () => {
     cy.contains('Биокотлета из марсианской Магнолии').click();
-    cy.contains('Детали ингредиента').should('exist');
+    cy.contains(ditals).should('exist');
     cy.get(closeModal).click();
-    cy.contains('Детали ингредиента').should('not.exist');
+    cy.contains(ditals).should('not.exist');
   });
 });
 
@@ -49,18 +52,30 @@ describe('Test order', () => {
       'ingredients'
     );
     cy.intercept('GET', 'api/auth/user', { fixture: 'userInfo.json' });
-    cy.visit(testUrl);
+    cy.visit('/');
+    cy.wait('@ingredients');
+    window.localStorage.setItem('accessToken', 'token');
   });
-  window.localStorage.setItem('accessToken', 'token');
+
+  afterEach(() => {
+    window.localStorage.removeItem('accessToken');
+    cy.clearCookies();
+    cy.clearLocalStorage();
+  });
   it('should create an order', () => {
-    cy.get(bun).contains('Добавить').click();
-    cy.get(sauce).contains('Добавить').click();
-    cy.get(main).contains('Добавить').click();
-    cy.get(ingredients).contains('Краторная булка N-200i').should('exist');
-    cy.get(ingredients)
-      .contains('Биокотлета из марсианской Магнолии')
-      .should('exist');
+    cy.get(bun).contains(add).click();
+    cy.get(sauce).contains(add).click();
+    cy.get(main).contains(add).click();
+    cy.get(ingredientsBurger).contains(bun).should('exist');
+    cy.get(ingredientsBurger).contains(main).should('exist');
+    cy.get(ingredientsBurger).contains(sauce).should('exist');
     cy.get('button[type=submit]').click();
-    cy.wait('@order');
+    cy.wait('@order').then((interception) => {
+      // Проверить данные в JSON-ответе
+      expect(interception.response.statusCode).to.equal(200);
+      expect(interception.response.body).to.have.property('order');
+      expect(interception.response.body.order).to.have.property('id');
+      expect(interception.response.body.order).to.have.property('ingredients');
+    });
   });
 });
